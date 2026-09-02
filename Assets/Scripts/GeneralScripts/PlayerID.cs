ad3d373b3ae57c6ec1f3b0edcd15ae0717a2c440
dshanzotoe-creator <darrenscotthanzotoe@gmail.com>
2026-09-02 20:51:29 +0200

    Create Player Prefab and Assign the Client PlayerID

---
+++ Assets/Scripts/GeneralScripts/PlayerID.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Unity.Multiplayer.PlayMode;
+using Unity.Netcode; 
+
+
+public class PlayerID : NetworkBehaviour
+{
+    [SerializeField] ulong clientOwnerID; 
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        clientOwnerID = OwnerClientId;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
