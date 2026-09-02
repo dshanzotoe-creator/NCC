0bf1a2f1c328322c16b68cfa721f8e88d451a0e5
dshanzotoe-creator <darrenscotthanzotoe@gmail.com>
2026-09-02 21:12:58 +0200

    Assign playerID to player prefab spawned in my server.

--- Assets/Scripts/GeneralScripts/PlayerID.cs
+++ Assets/Scripts/GeneralScripts/PlayerID.cs
@@ -8,6 +8,7 @@ public class PlayerID : NetworkBehaviour
     [SerializeField] ulong clientOwnerID; 
 
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
